0d1d74e7f924d883aaa7e1914fc9c98784214f71
Alexander <gaussmake1994@yandex.ru>
2017-04-01 02:39:04 +0300

    Version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='twitter_classifier',
-    version='0.1.4.8',
+    version='0.1.4.9',
     description='Watson-based Twitter sentiment classifier "API"',
     author='Alexander Pozharskiy',
     author_email='gaussmake@gmail.com',
